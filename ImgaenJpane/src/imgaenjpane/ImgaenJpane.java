a5a2df4b64853e9bd3ca4ed055f1e5a1f06d01aa
Yogsothoth <yogsothoth1984@gmail.com>
2019-06-22 19:49:02 -0300

    Proyecto final
    terminado por ahora

--- ImgaenJpane/src/imgaenjpane/ImgaenJpane.java
+++ ImgaenJpane/src/imgaenjpane/ImgaenJpane.java
@@ -1,9 +1,14 @@
 package imgaenjpane;
 
+import javax.swing.ImageIcon;
+
 public class ImgaenJpane {
 
     public static void main(String[] args) {
-        
+        Ventana1 ventana=new Ventana1();
+        ventana.setVisible(true);
+        ventana.setTitle("Imagen");//Ponerle un titulo
+        ventana.setLocationRelativeTo(null);//Centrado
     }
     
 }
